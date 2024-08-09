c8d219608781fc38aaf0524cabda4d6021029593
laynia <vc_mikuu@hotmail.com>
2024-08-09 17:50:32 +0200

    Fixing crash bomb

--- include/config.hpp
+++ include/config.hpp
@@ -3,5 +3,5 @@
 #include "config-utils/shared/config-utils.hpp"
 
 DECLARE_CONFIG(ModConfig,
-    CONFIG_VALUE(FullComboText, std::string, "Full Combo Text", "FULL COMBO!", "Changes the full combo text on the results screen.");
+    CONFIG_VALUE(FullComboText, std::string, "FULL COMBO!", "Changes the full combo text on the results screen.");
 )
